078cfcac983d7f87f0819ef1a4a2b5e6dd32b0a9
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-03-04 18:27:45 -0500

    adding client class

---
+++ src/com/client/AppClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.client;
+
+/**
+ * @author Sudhansu
+ *
+ */
+public class AppClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
