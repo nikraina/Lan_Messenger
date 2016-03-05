572aa88e36828e0d37f3469c50645ae36189a48f
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-03-04 19:29:03 -0500

    add AppClient and AppServer file

---
+++ src/com/server/AppServer.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.server;
+
+import java.io.PrintWriter;
+import java.net.*;
+
+/**
+ * @author Sudhansu
+ *
+ */
+public class AppServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AppServer appServer = new AppServer();
+		appServer.createServerConnection();
+
+	}
+	
+	void createServerConnection(){
+		String data = "Hello World!!";
+		try{
+			ServerSocket serSocket = new ServerSocket(4322);
+			Socket socket = serSocket.accept();
+			System.out.println("Server connected");
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			System.out.println("Sending data "+data);
+			out.print(data);
+			out.close();
+			socket.close();
+			serSocket.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Holy Crap!");
+		}
+	}
+}
